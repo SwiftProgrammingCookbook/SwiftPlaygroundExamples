964c9bf1bea6f37000e3b1164c3f341d4b88e753
Keith Moon <keef@keefmoon.com>
2017-05-31 23:58:14 +0100

    Embedded Resource

--- EmbeddedResource.playground/Contents.swift
+++ EmbeddedResource.playground/Contents.swift
@@ -19,13 +19,36 @@ struct Bar {
     var color: Color
 }
 
+class BarView: UIView {
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        backgroundColor = color
+        setupTexture()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = UIColor.red
+        setupTexture()
+    }
+    
+    private func setupTexture() {
+        let textureImage = UIImage(named: "texture")!
+        let textureColor = UIColor(patternImage: textureImage)
+        let textureView = UIView(frame: CGRect(origin: .zero, size: bounds.size))
+        textureView.backgroundColor = textureColor
+        addSubview(textureView)
+    }
+}
+
 class BarChart: UIView {
     
     var bars: [Bar] = [] {
         didSet {
             self.barViews.forEach { $0.removeFromSuperview() }
             
-            var barViews = [UIView]()
+            var barViews = [BarView]()
             
             let barCount: Int = bars.count
             
@@ -40,8 +63,7 @@ class BarChart: UIView {
                 let width = (frame.size.width - (interBarMargin * CGFloat(barCount+1))) / CGFloat(barCount)
                 let height = barHeight(forValue: bar.value)
                 let rect = CGRect(x: xOrigin, y: bounds.height - height, width: width, height: height)
-                let view = UIView(frame: rect)
-                view.backgroundColor = bar.color.displayColor
+                let view = BarView(frame: rect, color: bar.color.displayColor)
                 barViews.append(view)
                 addSubview(view)
                 
@@ -52,7 +74,7 @@ class BarChart: UIView {
     }
     var interBarMargin: CGFloat = 5.0
     
-    private var barViews: [UIView] = []
+    private var barViews: [BarView] = []
     private var maxValue: Float = 0.0
     
     private func barHeight(forValue value: Float) -> CGFloat {
@@ -62,7 +84,6 @@ class BarChart: UIView {
 
 let barView = BarChart(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 barView.backgroundColor = .white
-PlaygroundPage.current.liveView = barView
 
 let bar1 = Bar(value: 20, color: Color(red: 1, green: 0, blue: 0))
 let bar2 = Bar(value: 40, color: Color(red: 0, green: 1, blue: 0))
@@ -70,3 +91,4 @@ let bar3 = Bar(value: 25, color: Color(red: 0, green: 0, blue: 1))
 
 barView.bars = [bar1, bar2, bar3]
 
+PlaygroundPage.current.liveView = barView
